042f00e0aa7b89e4f5376a7a74b2885e3705aff8
A Mango Plantation <87632884+mangojoa@users.noreply.github.com>
2022-04-21 15:54:31 +0900

    Update InitializeDatabase.sh

--- scripts/InitializeDatabase.sh
+++ scripts/InitializeDatabase.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cat << EOF | PGPASSWORD=kRWQx1RCaXlUWUpnaDm3yvwU-h.RSk psql -h locallibrary-prod.crmzivx6ztt5.ap-northeast-2.rds.amazonaws.com -U postres
+cat << EOF | PGPASSWORD=kRWQx1RCaXlUWUpnaDm3yvwU-h.RSk psql -h locallibrary-prod.crmzivx6ztt5.ap-northeast-2.rds.amazonaws.com -U postgres
 CREATE DATABASE local_library;
 CREATE USER local_library WITH PASSWORD 'asdf1234';
 GRANT ALL PRIVILEGES ON DATABASE local_library TO local_library;
